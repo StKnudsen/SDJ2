6127838d523ac3e666fb75d62b44b98bb7493a38
StKnudsen <st.knudsen@gmail.com>
2021-03-03 17:14:48 +0100

    Exercise 11.5 noget..

--- Lesson11/e11-5_SimpleChat/src/client/RunClient.java
+++ Lesson11/e11-5_SimpleChat/src/client/RunClient.java
@@ -1,14 +1,25 @@
 package client;
 
 import client.networking.SocketClient;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class RunClient
+public class RunClient extends Application
 {
   public static void main(String[] args) throws IOException
   {
+    //  Start client networking
     SocketClient socketClient = new SocketClient();
     socketClient.start();
+
+    //  View
+
+  }
+
+  @Override public void start(Stage stage) throws Exception
+  {
+
   }
 }
